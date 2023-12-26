e43596541c3e9316c1dac43b04b2bac4f4a5c6d0
TanishaBageshwari <153014289+TanishaBageshwari@users.noreply.github.com>
2023-12-26 14:57:38 +0530

    Create 4.B sting handling.c

---
+++ Assignment part2/4.B sting handling.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+char str[100];
+int len;
+  printf("\nEnter the String : ");
+    scanf("%s",str);
+
+  
+len = strlen(str);
+   printf("\nLength of the given string is %d", len);
+  
+return(0);
+}
